388f27a2ffcac964a7bcabd95d8928f836b22958
joaoanitelli <133887229+joaoanitelli@users.noreply.github.com>
2024-03-14 19:00:54 -0300

    Add files via upload

---
+++ Site inspirato/mobile-navbar.js
@@ -0,0 +1,41 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = '<i class="fa-solid fa-x" style="color: #ffffff;"></i>';
+    }else {
+        menuMobile.classList.add('open');
+    }
+}function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = '<i class="fa-solid fa-x" style="color: #ffffff;"></i>';
+    }else {
+        menuMobile.classList.add('open');
+    }
+}function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = '<i class="fa-solid fa-x" style="color: #ffffff;"></i>';
+    }else {
+        menuMobile.classList.add('open');
+    }
+}function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = '<i class="fa-solid fa-x" style="color: #ffffff;"></i>';
+    }else {
+        menuMobile.classList.add('open');
+    }
+}function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = '<i class="fa-solid fa-x" style="color: #ffffff;"></i>';
+    }else {
+        menuMobile.classList.add('open');
+    }
+}
